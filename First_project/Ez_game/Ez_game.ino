9efd59d89e1397b6c8c062603f90e9d5153a6d55
donkzwonkz <donkzwonkz@gmail.com>
2019-08-12 17:47:40 +0800

    Preliminary code for first project
    sets pins, basic rng and accel readings

--- First_project/Ez_game/Ez_game.ino
+++ First_project/Ez_game/Ez_game.ino
@@ -1,18 +1,30 @@
 #include <SparkFun_ADXL345.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(3,OUTPUT)
-  pinMode(4,OUTPUT)
-  pinMode(5,OUTPUT)
-  pinMode(6,OUTPUT)
-  pinMode(7,OUTPUT)
-  pinMode(8,OUTPUT)
-  pinMode(9,OUTPUT)
+  
+  //set the sound pins
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
 
+  //dice rolling function with sound playing
+  void rollDice(){
+    int k = random(3, 8); //adjust based on the numbers of the pins required
+    digitalWrite(k,LOW)
+     
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int x,y,z
+  //variables to store acceleration
+  int x,y,z;
+  int accel;
+  adxl.readAccel(&x, &y, &z);
+  accel = x^2+y^2+z^2;
+  if (accel>5){ // nyumber is tentative
+    rollDice()
+    } 
 }
