665b15607946352d76bcce1433c9908dd8e18f2f
donkzwonkz <donkzwonkz@gmail.com>
2019-08-12 17:26:13 +0800

    Initial commit

---
+++ Ez_game/Ez_game.ino
@@ -0,0 +1,18 @@
+#include <SparkFun_ADXL345.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3,OUTPUT)
+  pinMode(4,OUTPUT)
+  pinMode(5,OUTPUT)
+  pinMode(6,OUTPUT)
+  pinMode(7,OUTPUT)
+  pinMode(8,OUTPUT)
+  pinMode(9,OUTPUT)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x,y,z
+}
